b54b02c940633164ee4a4b7a5f50742a1621cd6e
e1039daq <167813669+e1039daq@users.noreply.github.com>
2024-05-13 15:48:30 -0500

    Confirm working dir

--- beam/entrypoint.sh
+++ beam/entrypoint.sh
@@ -1,5 +1,6 @@
 # touch /root/.Xauthority
 # xauth add XAUTHKEY
-. /home/e1039daq/BeamDAQ/setup_slowcontrols.sh
-. /home/e1039daq/BeamDAQ/setup.sh
-/home/e1039daq/BeamDAQ/scripts/run_beamDAQ_forever.sh
+pwd
+. setup_slowcontrols.sh
+. setup.sh
+./scripts/run_beamDAQ_forever.sh
