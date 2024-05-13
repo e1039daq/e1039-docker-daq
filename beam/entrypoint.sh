36e78a04f0bec7996a50128c73817f24f8549074
e1039daq <167813669+e1039daq@users.noreply.github.com>
2024-05-13 16:27:18 -0500

    Update to Source

--- beam/entrypoint.sh
+++ beam/entrypoint.sh
@@ -3,4 +3,4 @@
 cd /home/e1039daq/BeamDAQ
 source /home/e1039daq/BeamDAQ/setup_slowcontrols.sh
 source /home/e1039daq/BeamDAQ/setup.sh
-/home/e1039daq/BeamDAQ/scripts/run_beamDAQ_forever.sh
+source /home/e1039daq/BeamDAQ/scripts/run_beamDAQ_forever.sh
