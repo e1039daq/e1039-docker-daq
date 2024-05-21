fa8cfd34a36ab8bf6c2a152ca2ea47a786811299
e1039daq <167813669+e1039daq@users.noreply.github.com>
2024-05-21 14:00:28 -0500

    Add wiat for GUI Scaler

--- scaler/entrypoint.sh
+++ scaler/entrypoint.sh
@@ -27,9 +27,14 @@ cd /home/$FTP_USER
 umask 002
 /usr/bin/crontab /opt/e1039sc4.e1039daq.crontab
 source dosetupcoda261
+echo " ------ Starting PreCoda ----- "
 ./PreCoda
-# echo "Starting RunFFT"
-xterm -hold -cr blue -bg blue -fg white -sb -rightbar -geometry 62x18+610+550 -sl 2500  -j  -T "Run FFT" -e './RunFFT'   &
+sleep 5
+echo " ------ Starting rcgui ----- "
 rcgui &
+echo " ------ Starting RunFFT ----- "
+xterm -hold -cr blue -bg blue -fg white -sb -rightbar -geometry 62x18+610+550 -sl 2500  -j  -T "Run FFT" -e './RunFFT'   &
+echo " ------ Starting Control Window ----- "
 /usr/local/bin/simple-gtk-buttonbox "Scaler DAQ Control" "PreCoda" "./PreCoda" "RCGui" "rcgui &" "RunFFT" "xterm -hold -cr blue -bg blue -fg white -sb -rightbar -geometry 62x18+610+550 -sl 2500  -j  -T 'Run FFT' -e './RunFFT'   &" "Kill Coda" "./kcoda" "Terminal" "xterm -hold -cr black -bg black -fg white -sb -rightbar -geometry 62x18+610+550 -sl 2500  -j  -T 'Scaler DAQ' -e 'tcsh' &" "Telnet ROC6SC" "xterm -hold -cr black -bg black -fg white -sb -rightbar -geometry 62x18+610+550 -sl 2500  -j  -T 'ROC6SC' -e 'telnet 192.168.24.101 100' &" 
+echo " ------ Stopping CODA ----- "
 ./kcoda
